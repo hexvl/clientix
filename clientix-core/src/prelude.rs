bd1d6f8a9c1f0631a35f3de9f649bbbce01467f0
Vladislav Ovchinnikov <vladovchinnikov950@gmail.com>
2025-09-06 00:50:00 +0300

    fix: fix serde

--- clientix-core/src/prelude.rs
+++ clientix-core/src/prelude.rs
@@ -1,8 +1,8 @@
 pub use reqwest;
 pub use serde;
-pub use serde_derive;
 pub use serde_json;
 pub use serde_urlencoded;
+pub use serde_xml_rs;
 pub use strfmt;
 pub use base64;
 pub use futures_util;
